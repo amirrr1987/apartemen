9bac38540b4347c26d85ac5606277995308361b1
Amir Maghami <Maghami.A1987@gmail.com>
2026-09-24 19:56:13 +0330

    chore: update dependencies in pnpm-lock.yaml and add new pnpm-lockss.yaml file

--- src/components/InfoTip.vue
+++ src/components/InfoTip.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import { onClickOutside } from '@vueuse/core'
-import { createPopper, type Instance } from '@popperjs/core'
-import { nextTick, onBeforeUnmount, ref } from 'vue'
+import { ref } from 'vue'
 import AppIcon from './AppIcon.vue'
 
 const props = defineProps<{
@@ -9,35 +8,20 @@ const props = defineProps<{
 }>()
 
 const open = ref(false)
-const trigger = ref<HTMLButtonElement | null>(null)
-const tooltip = ref<HTMLElement | null>(null)
 const root = ref<HTMLElement | null>(null)
-let popper: Instance | null = null
 
 onClickOutside(root, () => {
   open.value = false
 })
 
-async function toggle() {
+function toggle() {
   open.value = !open.value
-  await nextTick()
-  popper?.destroy()
-  popper = null
-  if (open.value && trigger.value && tooltip.value) {
-    popper = createPopper(trigger.value, tooltip.value, {
-      placement: 'top',
-      modifiers: [{ name: 'offset', options: { offset: [0, 8] } }],
-    })
-  }
 }
-
-onBeforeUnmount(() => popper?.destroy())
 </script>
 
 <template>
   <span ref="root" class="info-tip">
     <button
-      ref="trigger"
       class="info-tip-btn"
       type="button"
       aria-label="راهنمای این فیلد"
@@ -46,7 +30,7 @@ onBeforeUnmount(() => popper?.destroy())
     >
       <AppIcon name="info-circle" size="sm" />
     </button>
-    <div v-show="open" ref="tooltip" class="info-tip-box" role="tooltip">
+    <div v-show="open" class="info-tip-box" role="tooltip">
       {{ props.text }}
     </div>
   </span>
